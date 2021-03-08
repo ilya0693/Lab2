16c8cbc899f2bfc715be4bdb4c36054d5fd7bbac
Ilya <ilya0693@yandex.ru>
2021-03-08 21:39:40 +0300

    Created arithmetic.py and numbers.py files with code

---
+++ PyCharm/arithmetic.py
@@ -0,0 +1,5 @@
+answerUser = int(input("Enter the answer of exaple '4*400-54': " ))
+answerRight = int(4*100-54)
+
+print("User answer:", answerUser, "\n"
+"Right answer:", answerRight)
---
+++ PyCharm/numbers.py
@@ -0,0 +1,11 @@
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+c = input("Enter the third number: ")
+d = input("Enter the fourth number: ")
+
+E = float(a+b)
+F = float(c+d)
+
+G = E/F
+
+print("sum_a+b: %.2f, sum_c+d: %.2f, division: %.2f" % (E, F, G))
