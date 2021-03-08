546bcac12f225f8eb82fe2b211f9bef88fe15983
Ilya <ilya0693@yandex.ru>
2021-03-08 21:50:13 +0300

    Created arithmetic.py and numbers.py files with code

--- PyCharm/numbers.py
+++ PyCharm/numbers.py
@@ -1,11 +1,11 @@
-a = input("Enter the first number: ")
-b = input("Enter the second number: ")
-c = input("Enter the third number: ")
-d = input("Enter the fourth number: ")
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+c = float(input("Enter the third number: "))
+d = float(input("Enter the fourth number: "))
 
-E = float(a+b)
-F = float(c+d)
+E = a+b
+F = c+d
 
 G = E/F
 
-print("sum_a+b: %.2f, sum_c+d: %.2f, division: %.2f" % (E, F, G))
+print("sum_a+b: %.2f,\n sum_c+d: %.2f,\n division: %.2f" % (E, F, G))
