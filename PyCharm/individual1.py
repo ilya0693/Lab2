1c4f0661681a924ba7c7578223723453a5214d70
Ilya <ilya0693@yandex.ru>
2021-03-08 22:51:52 +0300

    Created individual.py files with code

---
+++ PyCharm/individual1.py
@@ -0,0 +1,8 @@
+a = float(input("Enter the lenght: "))
+b = float(input("Enter the width: "))
+
+P = 2*(a+b)
+import math
+d = math.sqrt(math.pow(a, 2) + math.pow(b, 2))
+
+print("Perimeter: ", P, "\n""Diagonal: ", d)
