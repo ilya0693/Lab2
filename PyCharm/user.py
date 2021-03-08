c130ed3e0696c740a4485b9065ef4102a2a19144
Ilya <ilya0693@yandex.ru>
2021-03-08 20:28:08 +0300

    Created user.py file with code

---
+++ PyCharm/user.py
@@ -0,0 +1,7 @@
+nameUser = input("What is your name? " )
+ageUser = input("How old are you? " )
+liveUser = input("Where are you live? ")
+
+print("This is {0}.\n"
+      "It is {1}.\n"
+      "(S)he lives in {2}".format(nameUser, ageUser, liveUser))
